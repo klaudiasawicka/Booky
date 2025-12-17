2cb19ede4cf0241b7587e649d53cf894ca0d7e3e
Hahatek <piotrus.guzowski@gmail.com>
2025-12-17 22:43:25 +0100

    Dodanie react-router do przechodzenia między stronami

--- BookyBuddy/src/App.jsx
+++ BookyBuddy/src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Button from "./components/ui/Button";
+import Card from "./components/Card";
+
 function App() {
   return (
-    <>
+    <div className="flex flex-col w-full h-full justify-center items-center">
       <Button text={"siema"} />
-    </>
+      <Card />
+    </div>
   );
 }
 
