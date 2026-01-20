12d549ce93cf123db5bd09918e6d3ead4f5ae815
andriybriliant <andrii.bryliant.inq@gmail.com>
2026-01-20 18:13:22 +0100

    Removed duplicate exceptions and GlobalExceptionHandle

--- backend/src/main/java/com/bookbuddy/bookbuddy/extension/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bookbuddy.bookbuddy.extension;
-
-public class BookNotFoundException extends RuntimeException{
-    public BookNotFoundException(String id){
-        super("Book not found with id: " + id);
-    }
-}
