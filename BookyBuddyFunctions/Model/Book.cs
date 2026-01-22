c621530f0ab06a30ac82718af82d169b05c61c36
klaudia <ksawicka2004.ks@gmail.com>
2026-01-23 00:06:11 +0100

    setting up the model for azure functions

---
+++ BookyBuddyFunctions/Model/Book.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookyBuddyFunctions.Model
+{
+    public class Book
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("title")]
+        public string Title { get; set; }
+        [BsonElement("avgRating")]
+        public double AvgRating { get; set; }
+        [BsonElement("ratingsCount")]
+        public int RatingsCount { get; set; }
+
+    }
+}
